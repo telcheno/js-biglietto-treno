908c6c7b9f27f9839926a6a5706e2988df41d7f2
telmo-checa <telmocheca1@gmail.com>
2024-03-09 13:04:34 +0100

    ADD: prove su html e css s pseudo codice nel app.js

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// Chiedere al utente la DISTANZA da percorrere
+  // Trasformare il dato dal "prompt" da "string" a "number" per potere fare i calcoli
+    //Stampa nella console
+// Chiedere l'ETA del utente
+  // Trasformare il dato dal "prompt" da "string" a "number" per potere fare i calcoli
+    //Stampa nella console
+// Calcolare il PREZZO TOTALE del biglieto del viaggio a seconda le seguenti condizioni:
+  // -Dichiarare il PREZZO BASE del Km (0.20€ al Km)
+    //Arotonda il PREZZO BASE con solo 2 decimali come centesimi
+    //Stampa nella console
+  // -Dichiara lo SCONTO per i MINORENI di 20% dal PREZZO TOTALE
+    //Stampa nella console
+  // -Dichiara lo SCONTO per i OVER 65 di 40% dal PREZZO TOTALE
+    //Stampa nella console
+  
+
